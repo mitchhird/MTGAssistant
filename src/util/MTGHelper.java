53bedf5bd0930a8b3eacd57583ff708bb3f232a1
Mitchell <mitchhird@gmail.com>
2017-01-29 11:31:12 -0500

    Check in resources for a future item.

--- src/util/MTGHelper.java
+++ src/util/MTGHelper.java
@@ -1,11 +1,17 @@
 package util;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import models.cardModels.Card;
 
@@ -13,6 +19,34 @@ import models.cardModels.Card;
  * Static Helper Class That Helps Varying Parts Of The Code Base 
  */
 public class MTGHelper {
+  
+  private static BufferedImage mtgSymbolSpriteSheet;
+  private static Map<String, BufferedImage> textToManaSymbolMap;
+  
+  // Initializes The Sprite Sheet If It Is Needed
+  private static void initMTGSymbolSpriteSheet () {
+    if (mtgSymbolSpriteSheet == null) {
+      try {
+        Path fileToLoad = Paths.get("external_resources/Mana.png");
+        mtgSymbolSpriteSheet = ImageIO.read(fileToLoad.toFile());
+        initTextManaSymbolMap();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  
+  //
+  private static void initTextManaSymbolMap () {
+    textToManaSymbolMap = new HashMap<String, BufferedImage>();
+    textToManaSymbolMap.put("{U}", mtgSymbolSpriteSheet.getSubimage(500, 200, 100, 100));
+  }
+  
+  public static BufferedImage getImageForManaSymbol (String manaSymbol) {
+    initMTGSymbolSpriteSheet();
+    return (textToManaSymbolMap.containsKey(manaSymbol)) ? textToManaSymbolMap.get(manaSymbol) : textToManaSymbolMap.get("{U}");
+  }
+  
   public static String generateCardKey (Card incomingCard) {
     return incomingCard.getName();
   }
